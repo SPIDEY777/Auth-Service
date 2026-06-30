8dddebcfee5b404e94cd8060cfb27a4e38a6d112
Spidey777 <s733749@gmail.com>
2026-06-30 17:58:32 +0530

    initalized project

--- auth/src/test/java/com/example/auth/AuthApplicationTests.java
+++ auth/src/test/java/com/example/auth/AuthApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.AuthService;
+package com.example.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuthServiceApplicationTests {
+class AuthApplicationTests {
 
 	@Test
 	void contextLoads() {
