fe9a213663019aeb0870506719407e696d33404a
Spidey777 <s733749@gmail.com>
2026-07-04 17:03:29 +0530

    security config and user config added

--- auth/src/main/java/com/example/auth/config/SecurityConfig.java
+++ auth/src/main/java/com/example/auth/config/SecurityConfig.java
@@ -1,14 +1,29 @@
 package com.example.auth.config;
 
 
+import com.example.auth.service.UserServiceDetailsImpl;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
-
+@Configuration
+@EnableMethodSecurity
+@Data
 public class SecurityConfig {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserServiceDetailsImpl userDetailsService;
+
+
 
 
 
