7203e72afb144912b48e683187b9d30b850a9dfe
Spidey777 <s733749@gmail.com>
2026-07-04 16:15:26 +0530

    service layer done repository layer done and a dto made for user sign up user functionality completed

--- auth/src/main/java/com/example/auth/service/JwtService.java
+++ auth/src/main/java/com/example/auth/service/JwtService.java
@@ -2,16 +2,19 @@ package com.example.auth.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Base64;
 import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
-public class Jwt {
+public class JwtService {
 
     public static final String SECRET =
             "EPPDb1fhYcvRcpZZmKl9m9vG7yzxZi4spwrSYGchqd0";
@@ -20,15 +23,35 @@ public class Jwt {
         return extractClaim(token, Claims::getSubject);
     }
 
-    public Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
-    }
+
 
     public <T> T extractClaim(String token,
                               Function<Claims, T> claimsResolver) {
 
         Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+
+    public Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private String createToken(Map<String,Object> claims,String username) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date (System.currentTimeMillis()))
+                .setExpiration(new Date (System.currentTimeMillis()+1000*60*1))
+                .signWith(getSignKey(),SignatureAlgorithm.HS256).compact();
+
     }
 
     private Claims extractAllClaims(String token) {
