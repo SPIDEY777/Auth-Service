c7996036fe787ff786f7c5e273e82a8faf777ae4
Spidey777 <s733749@gmail.com>
2026-07-08 15:44:32 +0530

    controller layer added

---
+++ auth/src/main/java/com/example/auth/controller/AuthController.java
@@ -0,0 +1,49 @@
+package com.example.auth.controller;
+
+import com.example.auth.entities.RefreshToken;
+import com.example.auth.model.UserInfoDTO;
+import com.example.auth.response.JwtResponseDTO;
+import com.example.auth.service.JwtService;
+import com.example.auth.service.RefreshTokenService;
+import com.example.auth.service.UserServiceDetailsImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+public class AuthController
+{
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private RefreshTokenService refreshTokenService;
+
+    @Autowired
+    private UserServiceDetailsImpl userDetailsService;
+
+    @PostMapping("auth/v1/signup")
+    public ResponseEntity SignUp(@RequestBody UserInfoDTO userInfoDto){
+        try{
+            Boolean isSignUped = userDetailsService.signUpUser(userInfoDto);
+            if(Boolean.FALSE.equals(isSignUped)){
+                return new ResponseEntity<>("Already Exist", HttpStatus.BAD_REQUEST);
+            }
+            RefreshToken refreshToken = refreshTokenService.createRefreshToken(userInfoDto.getUsername());
+            String jwtToken = jwtService.GenerateToken(userInfoDto.getUsername());
+            return new ResponseEntity<>(JwtResponseDTO.builder().accessToken(jwtToken).
+                    token(refreshToken.getToken()).build(), HttpStatus.OK);
+        }catch (Exception ex){
+            return new ResponseEntity<>("Exception in User Service", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
